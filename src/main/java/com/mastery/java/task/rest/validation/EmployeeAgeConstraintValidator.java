5c08ec51d5b7cc5d1cf97aadb5a6483e9da1125b
Oksana Pervak <o.pervak@bk.ru>
2021-09-30 00:55:37 +0300

    add jpa

---
+++ src/main/java/com/mastery/java/task/rest/validation/EmployeeAgeConstraintValidator.java
@@ -0,0 +1,17 @@
+package com.mastery.java.task.rest.validation;
+
+import java.time.LocalDate;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.mastery.java.task.dto.annatation.EmployeeAgeConstraint;
+
+public class EmployeeAgeConstraintValidator implements ConstraintValidator<EmployeeAgeConstraint, LocalDate>{
+
+	@Override
+	public boolean isValid(LocalDate dateBirth, ConstraintValidatorContext context) {
+		if (dateBirth == null) return false;
+		return LocalDate.now().minusYears(18).isAfter(dateBirth);
+	}
+
+}
---
+++ src/main/java/com/mastery/java/task/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.mastery.java.task.service;
+
+import java.util.Optional;
+
+import com.mastery.java.task.dto.Employee;
+
+public interface EmployeeService {
+	
+	Iterable<Employee> getAll();
+
+	Optional<Employee> getById(long id);
+
+	Employee create(Employee employee);
+
+	Employee update(long id, Employee employee);
+
+	void delete(long id);
+
+
+}
